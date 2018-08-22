9e26cf8dc61e8976fa13257c81f2f1127a2d54b0
Uku.Tammet <uku.tammet@ttu.ee>
2018-08-22 17:23:22 +0300

    Use new mechanism for rendering

--- src/message/Message.js
+++ src/message/Message.js
@@ -3,9 +3,22 @@ import PropTypes from 'prop-types';
 
 import { Consumer as ContextConsumer } from '../common/context';
 
-const Message = ({ children, params }) => (
+const Message = ({ children, params, dangerouslyTranslateInnerHTML }) => (
   <ContextConsumer>
-    {({ translations }) => translations.translate(children, params)}
+    {({ translations }) => {
+      if (!dangerouslyTranslateInnerHTML) {
+        return translations.translate(children, params).map(translation => translation.value);
+      }
+      return translations.translate(dangerouslyTranslateInnerHTML, params).map(
+        translation =>
+          !translation.dangerous ? (
+            // eslint-disable-next-line react/no-danger
+            <span dangerouslySetInnerHTML={{ __html: translation.value }} />
+          ) : (
+            translation.value
+          ),
+      );
+    }}
   </ContextConsumer>
 );
 
@@ -13,6 +26,7 @@ Message.displayName = 'Message';
 Message.propTypes = {
   children: PropTypes.string,
   params: PropTypes.objectOf(PropTypes.node),
+  dangerouslyTranslateInnerHTML: PropTypes.string,
 };
 
 export default Message;
