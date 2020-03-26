4ba57881fb0d8f10a6eaa15f89934bfa937911f9
Stephen Brandwood <stephen.brandwood@transferwise.com>
2020-03-26 17:14:29 +0000

    Add Typescript declarations, alias Provider as TranslationProvider

---
+++ src/retranslate.d.ts
@@ -0,0 +1,43 @@
+import { ReactNode, FC, ComponentType } from 'react';
+
+type Parameters = Record<string, ReactNode>;
+
+type Translate = (key: string, parameters?: Parameters) => string;
+
+type TranslateAsParts = (
+  key: string,
+  parameters?: Parameters,
+) => [{ dangerous: boolean; value: string }];
+
+export interface Translations {
+  translate: Translate;
+  translateAsParts: TranslateAsParts;
+  language: string;
+}
+
+export interface ProviderProps {
+  children?: ReactNode;
+  language?: string;
+  fallbackLanguage: string;
+  messages: Record<string, Record<string, string>>;
+}
+export const Provider: FC<ProviderProps>;
+export const TranslationProvider: FC<ProviderProps>;
+
+export interface MessageProps {
+  children?: ReactNode;
+  params?: Parameters;
+  dangerouslyTranslateInnerHTML?: string;
+}
+export const Message: FC<MessageProps>;
+
+declare function useTranslations(): Translations;
+
+export interface WithTranslationProps {
+  translations: Translations;
+}
+declare function withTranslations<P extends WithTranslationProps>(
+  WrappedComponent: ComponentType<P>,
+): ReactNode;
+
+export const WithTranslations: FC<{ children: (translations: Translations) => ReactNode }>;
